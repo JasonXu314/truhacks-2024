66de83674a9841795feeb58e48363a7fb14ad43a
ewbyf <107148516+ewbyf@users.noreply.github.com>
2024-03-23 01:35:40 -0500

    init and some pages

---
+++ frontend/src/pages/signin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { FiArrowUpRight, FiStar } from 'react-icons/fi';
+import api from '@/services/axiosConfig';
+
+const SignIn = () => {
+	return (
+        <></>
+	);
+};
+export default SignIn;
---
+++ frontend/src/pages/student.tsx
@@ -0,0 +1,9 @@
+const Student = () => {
+    return (
+    <>
+        <p>Welcome to student hub</p>
+    </>
+    );
+}
+ 
+export default Student;
