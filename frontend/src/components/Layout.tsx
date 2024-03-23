c82f12cae9702f944f63ddec7628e06445fbb6c8
Aiturgan <aiturgan888@gmail.com>
2024-03-23 17:52:53 -0500

    signup

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -8,6 +8,7 @@ const Layout = ({ children }: { children: JSX.Element }) => {
 	const router = useRouter();
 
 	return (
+<<<<<<< Updated upstream
 		<>
 			<div className='bg-[#6567D6] opacity-[5%] fixed top-[-40%] left-[-5%]aspect-square w-[40%] rounded-full blur-lg'></div>
 			<div className='bg-[#D9D9D9] opacity-[18%] fixed top-[20%] right-[-5%] aspect-square w-[40%] rounded-full blur-lg'></div>
@@ -18,6 +19,17 @@ const Layout = ({ children }: { children: JSX.Element }) => {
 				</div>
 			</div>
 		</>
+=======
+		<div className={`h-full ${sans.className} flex flex-col`}>
+			<div className="bg-[#6567D6] opacity-[5%] fixed top-[-40%] left-[-5%]aspect-square w-[40%] rounded-full blur-lg"></div>
+			{!router.pathname.startsWith('/signin') && (
+				<div className="bg-[#D9D9D9] opacity-[18%] fixed top-[20%] right-[-5%] aspect-square w-[40%] rounded-full blur-lg"></div>
+			)}
+
+			{!router.pathname.startsWith('/session') && !router.pathname.startsWith('/signin') && !router.pathname.startsWith('/signup') && <Navbar />}
+			{children}
+		</div>
+>>>>>>> Stashed changes
 	);
 };
 
