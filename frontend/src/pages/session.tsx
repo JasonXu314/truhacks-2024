38ab9373d797c4c1e20b9b64fe7b2f6419cf2752
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2024-03-24 01:13:46 -0500

    fix

--- frontend/src/pages/session.tsx
+++ frontend/src/pages/session.tsx
@@ -64,6 +64,30 @@ const Session = () => {
 
 				socket.send(JSON.stringify({ event: 'CLAIM', data: { otp: router.query.otp } }));
 
+				let peer;
+				if (tutor) {
+					peer = new Peer({
+						initiator: false,
+						trickle: false,
+						stream: stream,
+					});
+				} else {
+					peer = new Peer({
+						initiator: true,
+						trickle: false,
+						stream: stream,
+					});
+				}
+
+				peer.on('signal', (data) => {
+					console.log(data);
+					socket.send(JSON.stringify({ event: 'SIGNAL', data: { signal: JSON.stringify(data) } }));
+				});
+
+				peer.on('stream', (stream) => {
+					partnerVideo.current.srcObject = stream;
+				});
+
 				socket.addEventListener(
 					'message',
 					(evt) => {
@@ -73,40 +97,14 @@ const Session = () => {
 							console.log('claim success');
 
 							socketRef.current = socket;
+						} else if (msg.type === 'SIGNAL') {
+							console.log(msg);
+							console.log(msg.data.signal);
 						}
-                        else if (msg.type === 'SIGNAL') {
-                            console.log(msg);
-                            console.log(msg.data.signal)
-                        }
 					},
 					{ once: true }
 				);
 			});
-
-            let peer;
-            if (tutor) {
-                peer = new Peer({
-                    initiator: false,
-                    trickle: false,
-                    stream: stream
-                });
-            }
-            else {
-                peer = new Peer({
-                    initiator: true,
-                    trickle: false,
-                    stream: stream
-                });
-            }
-
-            peer.on('signal', (data) => {
-                console.log(data);
-                socket.send(JSON.stringify({ event: 'SIGNAL', data: { signal: JSON.stringify(data) } }));
-            });
-    
-            peer.on('stream', (stream) => {
-                partnerVideo.current.srcObject = stream;
-            });
 
 			socket.addEventListener('error', (evt) => {
 				console.log('socket error', evt);
@@ -174,13 +172,13 @@ const Session = () => {
 		const peer = new Peer({
 			initiator: true,
 			trickle: false,
-			stream: stream
+			stream: stream,
 		});
 
 		peer.on('signal', (data) => {
 			console.log(data);
 
-            socketRef.current?.send(JSON.stringify(data));
+			socketRef.current?.send(JSON.stringify(data));
 
 			// api.post('/api/topics/offer', { token, data: JSON.stringify(data) })
 			// 	.then((res) => {
@@ -208,13 +206,13 @@ const Session = () => {
 		const peer = new Peer({
 			initiator: false,
 			trickle: false,
-			stream: stream
+			stream: stream,
 		});
 		peer.on('signal', (data) => {
 			console.log(data);
 			api.post('/api/topics/offer', {
 				data: JSON.stringify(data),
-				token
+				token,
 			}).then(() => {
 				setCallAccepted(true);
 				setOpen(false);
@@ -239,93 +237,93 @@ const Session = () => {
 
 	return (
 		<TooltipProvider delayDuration={150}>
-			<div className='flex flex-col w-3/5 mx-auto h-full min-h-screen gap-3 justify-center py-8'>
+			<div className="flex flex-col w-3/5 mx-auto h-full min-h-screen gap-3 justify-center py-8">
 				{tutor === false && <StudentModal isOpen={open} setIsOpen={setOpen} />}
 				{tutor === true && <TutorModal isOpen={open} setIsOpen={setOpen} />}
 				<EndSessionModal isOpen={openEndModal} setIsOpen={setOpenEndModal} tutor={tutor}></EndSessionModal>
-				<div className='flex items-center flex-row gap-2 absolute top-6 left-10'>
-					<img src='img/logo.png' alt='Logo EducateAll' width='36' height='36'></img>
-					<p className='font-bold text-xl text-blue'>EducateAll</p>
+				<div className="flex items-center flex-row gap-2 absolute top-6 left-10">
+					<img src="img/logo.png" alt="Logo EducateAll" width="36" height="36"></img>
+					<p className="font-bold text-xl text-blue">EducateAll</p>
 				</div>
-				<p className='text-text text-4xl font-bold text-center'>
-					Welcome, {name}, to your <span className='text-blue'>Tutoring Session!</span>
+				<p className="text-text text-4xl font-bold text-center">
+					Welcome, {name}, to your <span className="text-blue">Tutoring Session!</span>
 				</p>
-				<div className='flex border-[1px] border-blue rounded-lg bg-[#E1E1F6] h-screen z-[20]'>
-					<div className='h-full w-full p-4'>
+				<div className="flex border-[1px] border-blue rounded-lg bg-[#E1E1F6] h-screen z-[20]">
+					<div className="h-full w-full p-4">
 						{/* <div className='h-full w-full bg-white'></div> */}
 						<Canvas color={color} eraserEquipped={eraserEquipped} socketRef={socketRef} />
 					</div>
-					<div className='flex flex-col justify-between px-4 py-8 gap-4 w-1/3'>
+					<div className="flex flex-col justify-between px-4 py-8 gap-4 w-1/3">
 						<div>
 							<p>Eric Wong (Tutor)</p>
-							{stream && <video className='w-full border-2 border-blue border-solid' muted playsInline ref={userVideo} autoPlay />}
+							{stream && <video className="w-full border-2 border-blue border-solid" muted playsInline ref={userVideo} autoPlay />}
 						</div>
 						<div>
 							<p>Aiturgan Talant (Student)</p>
-							{callAccepted && <video className='w-full border-2 border-blue border-solid' playsInline ref={partnerVideo} autoPlay />}
+							{callAccepted && <video className="w-full border-2 border-blue border-solid" playsInline ref={partnerVideo} autoPlay />}
 						</div>
 					</div>
 				</div>
-				<div className='h-24 w-[95%] bg-blue mx-auto rounded-lg flex gap-6 items-center justify-center px-8'>
-					<div className='flex gap-2 border-r-[1.5px] h-full items-center px-8 flex-grow'>
+				<div className="h-24 w-[95%] bg-blue mx-auto rounded-lg flex gap-6 items-center justify-center px-8">
+					<div className="flex gap-2 border-r-[1.5px] h-full items-center px-8 flex-grow">
 						<div className={`${color === 'black' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-black rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-black rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('black')}
 							></div>
 						</div>
 
 						<div className={`${color === '#ef4444' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-red-500 rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-red-500 rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#ef4444')}
 							></div>
 						</div>
 
 						<div className={`${color === '#38bdf8' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-sky-400 rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-sky-400 rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#38bdf8')}
 							></div>
 						</div>
 
 						<div className={`${color === '#FF69B4' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-[#FF69B4] rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-[#FF69B4] rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#FF69B4')}
 							></div>
 						</div>
 
 						<div className={`${color === '#18A804' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-[#18A804] rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-[#18A804] rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#18A804')}
 							></div>
 						</div>
 
 						<div className={`${color === '#ff9a36' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-[#ff9a36] rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-[#ff9a36] rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#ff9a36')}
 							></div>
 						</div>
 
 						<div className={`${color === '#f8ff00' && !eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`}>
 							<div
-								className='h-11 w-11 bg-[#f8ff00] rounded-lg cursor-pointer selected:border-[1px] border-white'
+								className="h-11 w-11 bg-[#f8ff00] rounded-lg cursor-pointer selected:border-[1px] border-white"
 								onClick={() => changeColor('#f8ff00')}
 							></div>
 						</div>
 
 						<div className={`${eraserEquipped && 'border-b-[2.5px]'} p-2 cursor-pointer`} onClick={() => setEraserEquipped(true)}>
-							<BsEraserFill color='white' size={43}></BsEraserFill>
+							<BsEraserFill color="white" size={43}></BsEraserFill>
 						</div>
 					</div>
 
 					{!screenShareOn && (
 						<Tooltip>
 							<TooltipTrigger>
-								<LuScreenShare color='white' size={32} className='cursor-pointer flex-shrink-0' onClick={shareScreen}></LuScreenShare>
+								<LuScreenShare color="white" size={32} className="cursor-pointer flex-shrink-0" onClick={shareScreen}></LuScreenShare>
 							</TooltipTrigger>
 							<TooltipContent>
 								<p>Share screen</p>
@@ -333,21 +331,20 @@ const Session = () => {
 						</Tooltip>
 					)}
 					{screenShareOn && (
-                        <Tooltip>
-                        <TooltipTrigger>
-                        <LuScreenShareOff color='white' size={32} className='cursor-pointer flex-shrink-0' onClick={shareScreen}></LuScreenShareOff>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>Stop sharing</p>
-                        </TooltipContent>
-                    </Tooltip>
-						
+						<Tooltip>
+							<TooltipTrigger>
+								<LuScreenShareOff color="white" size={32} className="cursor-pointer flex-shrink-0" onClick={shareScreen}></LuScreenShareOff>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p>Stop sharing</p>
+							</TooltipContent>
+						</Tooltip>
 					)}
-					{cameraOn && <BsCameraVideo color='white' size={32} className='cursor-pointer flex-shrink-0' onClick={toggleCamera} />}
-					{!cameraOn && <BsCameraVideoOff color='#E14040' size={32} className='cursor-pointer flex-shrink-0' onClick={toggleCamera} />}
-					{micOn && <BsMic color='white' size={32} className='cursor-pointer flex-shrink-0' onClick={toggleMic} />}
-					{!micOn && <BsMicMute color='#E14040' size={32} className='cursor-pointer flex-shrink-0' onClick={toggleMic} />}
-					<Button onClick={() => endSession()} className='bg-white text-blue hover:bg-[#F2F2F2]'>
+					{cameraOn && <BsCameraVideo color="white" size={32} className="cursor-pointer flex-shrink-0" onClick={toggleCamera} />}
+					{!cameraOn && <BsCameraVideoOff color="#E14040" size={32} className="cursor-pointer flex-shrink-0" onClick={toggleCamera} />}
+					{micOn && <BsMic color="white" size={32} className="cursor-pointer flex-shrink-0" onClick={toggleMic} />}
+					{!micOn && <BsMicMute color="#E14040" size={32} className="cursor-pointer flex-shrink-0" onClick={toggleMic} />}
+					<Button onClick={() => endSession()} className="bg-white text-blue hover:bg-[#F2F2F2]">
 						End session
 					</Button>
 				</div>
